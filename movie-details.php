0dc23e68f1adcbd9e3878b75f393c858ba7869e1
Heitor312 <santoscostaheitor4@gmail.com>
2024-10-28 11:03:28 -0300

    Update movie-details.php

--- movie-details.php
+++ movie-details.php
@@ -99,9 +99,19 @@ $conn->close();
             <!-- Add a rating system -->
             <div class="rating">
                 <span>Rating:</span>
-                <span class="star-rating">
-                    <?php echo $movie['rating'] ?>
-                </span>
+                <form class="star-rating" method="POST" action="movie-details.php?id=<?php echo $movie['id']; ?>">
+                    <input type="radio" id="star5" name="rate" value="5">
+                    <label for="star5" title="text"> </label>
+                    <input type="radio" id="star4" name="rate" value="4">
+                    <label for="star4" title="text"> </label>
+                    <input type="radio" id="star3" name="rate" value="3">
+                    <label for="star3" title="text"> </label>
+                    <input type="radio" id="star2" name="rate" value="2">
+                    <label for="star2" title="text"> </label>
+                    <input type="radio" id="star1" name="rate" value="1">
+                    <label for="star1" title="text"> </label>
+                    <button type="submit">Enviar.</button>
+                </form>
             </div>
         </div>
         <?php if($movie['id'] <= 40): ?> <a href="ranking.php"><button>Voltar para Rankings</button></a>
