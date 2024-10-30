581194d6c14b3eac8730c8223c421efcfacb493c
Heitor312 <santoscostaheitor4@gmail.com>
2024-10-30 12:09:58 -0300

    Update movie-details.php

--- movie-details.php
+++ movie-details.php
@@ -102,16 +102,16 @@ $conn->close();
                 <span>Rating:</span>
                 <form class="star-rating" method="POST" action="movie-details.php?id=<?php echo $movie['id']; ?>">
                     <input type="radio" id="star5" name="rate" value="5">
-                    <label for="star5" title="text"> </label>
+                    <label for="star5" title="text">&#9733</label>
                     <input type="radio" id="star4" name="rate" value="4">
-                    <label for="star4" title="text"> </label>
+                    <label for="star4" title="text">&#9733</label>
                     <input type="radio" id="star3" name="rate" value="3">
-                    <label for="star3" title="text"> </label>
+                    <label for="star3" title="text">&#9733</label>
                     <input type="radio" id="star2" name="rate" value="2">
-                    <label for="star2" title="text"> </label>
+                    <label for="star2" title="text">&#9733</label>
                     <input type="radio" id="star1" name="rate" value="1">
-                    <label for="star1" title="text"> </label>
-                    <button type="submit">Enviar.</button>
+                    <label for="star1" title="text">&#9733</label>
+                    
                 </form>
             </div>
         </div>
