3348de1cc5feac1d8ad45618b42cea5cc8e2cfa1
Ultra-dev123 <azmguitar3@gmail.com>
2024-11-01 19:16:48 -0300

    fixed logout redirect to index

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy(); // Destrói a sessão
-header("Location: login.php"); // Redireciona para a página de login
+header("Location: index.php"); // Redireciona para a página de inicio
 exit;
 ?>
